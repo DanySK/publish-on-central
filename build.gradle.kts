b74f4575fddaca8e2b5a7b5f6fa56e487c32cd2b
Danilo Pianini <danilo.pianini@gmail.com>
2019-03-16 12:53:07 +0100

    Try to use the plugin for the plugin itself

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,14 @@ plugins {
     id("com.palantir.git-version") version gitVersionVersion
     kotlin("jvm") version kotlinVersion
     id("com.gradle.plugin-publish") version gradlePluginPublisVersion
+    id ("org.danilopianini.publish-on-central") version "0.1.0-archeo+14dc9b3"
 }
 
 group = "org.danilopianini"
 val projectId = "$group.$name"
 val fullName = "Gradle Publish On Maven Central Plugin"
 val websiteUrl = "https://github.com/DanySK/maven-central-gradle-plugin"
-val projectDescription = "A Plugin for easily publishing artifacts on Maven Central"
+val projectDetails = "A Plugin for easily publishing artifacts on Maven Central"
 val pluginImplementationClass = "org.danilopianini.gradle.mavencentral.PublishOnCentral"
 
 val versionDetails: VersionDetails = (property("versionDetails") as? Closure<VersionDetails>)?.call()
@@ -61,6 +62,14 @@ configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_6
 }
 
+publishOnCentral {
+    //    projectnaname.set(fullName)
+    projectDescription.set(projectDetails)
+    scmRepoName.set("maven-central-gradle-plugin")
+    repositoryURL.set(URI("sadaswdasdsa"))
+    println(scmRepoName)
+}
+
 tasks {
     "test"(Test::class) {
         useJUnitPlatform()
@@ -81,25 +90,25 @@ tasks {
             file("$outputDir/plugin-classpath.txt").writeText(sourceSets.main.get().runtimeClasspath.joinToString("\n"))
         }
     }
-    register<Jar>("sourcesJar") {
-        archiveClassifier.set("sources")
-        val sourceSets = project.properties["sourceSets"] as? SourceSetContainer
-            ?: throw IllegalStateException("Unable to get sourceSets for project $project. Got ${project.properties["sourceSets"]}")
-        val main = sourceSets.getByName("main").allSource
-        from(main)
-    }
-    register<Jar>("javadocJar") {
-        archiveClassifier.set("javadoc")
-        val javadoc = project.tasks.findByName("javadoc") as? Javadoc
-            ?: throw IllegalStateException("Unable to get javadoc task for project $project. Got ${project.task("javadoc")}")
-        from(javadoc.destinationDir)
-    }
+//    register<Jar>("sourcesJar") {
+//        archiveClassifier.set("sources")
+//        val sourceSets = project.properties["sourceSets"] as? SourceSetContainer
+//            ?: throw IllegalStateException("Unable to get sourceSets for project $project. Got ${project.properties["sourceSets"]}")
+//        val main = sourceSets.getByName("main").allSource
+//        from(main)
+//    }
+//    register<Jar>("javadocJar") {
+//        archiveClassifier.set("javadoc")
+//        val javadoc = project.tasks.findByName("javadoc") as? Javadoc
+//            ?: throw IllegalStateException("Unable to get javadoc task for project $project. Got ${project.task("javadoc")}")
+//        from(javadoc.destinationDir)
+//    }
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.6"
     }
-    withType<Sign> {
-        onlyIf { project.property("signArchivesIsEnabled")?.toString()?.toBoolean() ?: false }
-    }
+//    withType<Sign> {
+//        onlyIf { project.property("signArchivesIsEnabled")?.toString()?.toBoolean() ?: false }
+//    }
 }
 
 // Add the classpath file to the test runtime classpath
@@ -107,33 +116,34 @@ dependencies {
     testRuntimeOnly(files(tasks["createClasspathManifest"]))
 }
 
+
 publishing {
-    publications {
-        create<MavenPublication>("mavenCentral") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["java"])
-            artifact(project.property("sourcesJar"))
-            artifact(project.property("javadocJar"))
-            pom {
-                name.set(fullName)
-                description.set(projectDescription)
-                url.set(websiteUrl)
-                licenses {
-                    license {
-                        name.set("")
-                        url.set("")
-                    }
-                }
-                scm {
-                    url.set(this@pom.url.get())
-                    connection.set("git@github.com:DanySK/maven-central-gradle-plugin.git")
-                    developerConnection.set(connection.get())
-                }
-            }
-        }
-    }
+//    publications {
+//        create<MavenPublication>("mavenCentral") {
+//            groupId = project.group.toString()
+//            artifactId = project.name
+//            version = project.version.toString()
+//            from(components["java"])
+//            artifact(project.property("sourcesJar"))
+//            artifact(project.property("javadocJar"))
+//            pom {
+//                name.set(fullName)
+//                description.set(projectDetails)
+//                url.set(websiteUrl)
+//                licenses {
+//                    license {
+//                        name.set("")
+//                        url.set("")
+//                    }
+//                }
+//                scm {
+//                    url.set(this@pom.url.get())
+//                    connection.set("git@github.com:DanySK/maven-central-gradle-plugin.git")
+//                    developerConnection.set(connection.get())
+//                }
+//            }
+//        }
+//    }
     repositories {
         maven {
             url = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
@@ -145,9 +155,9 @@ publishing {
     }
 }
 
-configure<SigningExtension> {
-    sign(publishing.publications.getByName("mavenCentral"))
-}
+//configure<SigningExtension> {
+//    sign(publishing.publications.getByName("mavenCentral"))
+//}
 
 pluginBundle {
     website = websiteUrl
@@ -160,7 +170,7 @@ gradlePlugin {
         create(projectId) {
             id = projectId
             displayName = fullName
-            description = projectDescription
+            description = projectDetails
             implementationClass = pluginImplementationClass
         }
     }
