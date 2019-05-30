e4c3a2e2474126cdbbcbaa1708ea3b675fa534a7
Danilo Pianini <danilo.pianini@unibo.it>
2019-05-30 15:56:57 +0200

    Upgrade Kotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -19,9 +19,9 @@ object Versions {
 
     const val org_jetbrains_dokka_gradle_plugin: String = "0.9.18" 
 
-    const val org_jetbrains_kotlin_jvm_gradle_plugin: String = "1.3.21" // available: "1.3.31"
+    const val org_jetbrains_kotlin_jvm_gradle_plugin: String = "1.3.31" 
 
-    const val org_jetbrains_kotlin: String = "1.3.21" // available: "1.3.31"
+    const val org_jetbrains_kotlin: String = "1.3.31" 
 
     /**
      *
