f83dae01d8aedd729864dcb14429637f7904f40c
Danilo Pianini <danilo.pianini@unibo.it>
2019-05-30 15:55:17 +0200

    Upgrade Dokka

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -17,7 +17,7 @@ object Versions {
 
     const val org_danilopianini_publish_on_central_gradle_plugin: String = "0.1.1" 
 
-    const val org_jetbrains_dokka_gradle_plugin: String = "0.9.17" // available: "0.9.18"
+    const val org_jetbrains_dokka_gradle_plugin: String = "0.9.18" 
 
     const val org_jetbrains_kotlin_jvm_gradle_plugin: String = "1.3.21" // available: "1.3.31"
 
