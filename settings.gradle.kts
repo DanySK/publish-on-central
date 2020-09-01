5dc9bcd8f8650d179d7268d29f0a42b8767f66b7
Danilo Pianini <danilo.pianini@gmail.com>
2020-09-01 21:21:31 +0200

    Use newer refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-import de.fayard.dependencies.bootstrapRefreshVersionsAndDependencies
+import de.fayard.refreshVersions.bootstrapRefreshVersions
 import org.danilopianini.VersionAliases.justAdditionalAliases
 buildscript {
     repositories {
@@ -6,10 +6,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("de.fayard:dependencies:0.+")
-        classpath("org.danilopianini:refreshversions-aliases:0.+")
+        classpath("de.fayard.refreshVersions:refreshVersions:0.9.5")
+        classpath("org.danilopianini:refreshversions-aliases:+")
     }
 }
-bootstrapRefreshVersionsAndDependencies(justAdditionalAliases)
+bootstrapRefreshVersions(justAdditionalAliases)
 rootProject.name = "publish-on-central"
 
