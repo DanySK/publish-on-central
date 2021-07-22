0c47538d76f2e6e071fa1e8fd3931604d0bad07d
Danilo Pianini <danilo.pianini@gmail.com>
2021-07-22 17:49:59 +0200

    Fix UndocumentedPublicClass

--- src/main/kotlin/org.danilopianini.gradle.mavencentral/PublishOnCentral.kt
+++ src/main/kotlin/org.danilopianini.gradle.mavencentral/PublishOnCentral.kt
@@ -105,6 +105,10 @@ class PublishOnCentral : Plugin<Project> {
     }
 }
 
+/**
+ * A [Jar] task with the specified classifier, and adopting the duplicate strategy
+ * [org.gradle.api.file.DuplicatesStrategy.WARN].
+ */
 open class JarWithClassifier(classifier: String) : Jar() {
     init {
         archiveClassifier.set(classifier)
