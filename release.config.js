b9785cb49174999c3823f8af634b707e80e92045
Danilo Pianini <danilo.pianini@gmail.com>
2022-01-06 13:18:47 +0100

    fix(release): correctly enable semantic commit conventions

---
+++ release.config.js
@@ -0,0 +1,19 @@
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+git push --force origin \${nextRelease.version}
+./gradlew releaseKotlin
+./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET
+./gradlew publishKotlinMavenPublicationToGithubRepository || true
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+module.exports = config
