02355c477b56a82b8c82d581f778f492037f5f02
Danilo Pianini <danilo.pianini@unibo.it>
2022-06-09 16:24:29 +0200

    test: add verification for sources and javadoc jar generation

--- src/test/resources/org/danilopianini/gradle/test/test0/build.gradle.kts
+++ src/test/resources/org/danilopianini/gradle/test/test0/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.danilopianini.publish-on-central")
 }
 group = "io.github.danysk"
+version = "0.1.0"
 publishOnCentral {
     projectDescription.set("test")
     repository("https://maven.pkg.github.com/OWNER/REPOSITORY") {
