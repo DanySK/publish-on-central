c6023283f642fb67a50422e609f867e2394ab3af
Danilo Pianini <danilo.pianini@unibo.it>
2022-06-30 17:01:49 +0200

    refactor: extract MavenRepositoryDescriptor to its own source file

--- src/main/kotlin/org/danilopianini/gradle/mavencentral/PublishOnCentralExtension.kt
+++ src/main/kotlin/org/danilopianini/gradle/mavencentral/PublishOnCentralExtension.kt
@@ -1,8 +1,8 @@
 package org.danilopianini.gradle.mavencentral
 
+import MavenRepositoryDescriptor
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
-import org.gradle.kotlin.dsl.property
 import java.time.Duration
 
 /**
@@ -118,38 +118,4 @@ open class PublishOnCentralExtension(val project: Project) {
 
         private fun repositoryNameFromURL(url: String) = extractName.find(url)?.destructured?.component1() ?: "unknown"
     }
-}
-
-/**
- * A descriptor of a Maven repository.
- * Requires a [name], and optionally authentication in form of [user] and [password].
- */
-class MavenRepositoryDescriptor internal constructor(
-    project: Project,
-    var name: String,
-) {
-    /**
-     * The username.
-     */
-    val user: Property<String> = project.objects.property()
-
-    /**
-     * The password.
-     */
-    val password: Property<String> = project.objects.property()
-
-    /**
-     * The Nexus URL, if installed.
-     */
-    var nexusUrl: String? = null
-
-    /**
-     * The Nexus timeout.
-     */
-    var nexusTimeOut: Duration = Duration.ofMinutes(1)
-
-    /**
-     * The Nexus connection timeout.
-     */
-    var nexusConnectionTimeout: Duration = nexusTimeOut
 }
