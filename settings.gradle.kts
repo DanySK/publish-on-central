4afc614f4b57463e2a74e8d0c40a580b567c5cd7
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-17 14:39:50 +0100

    build: apply the foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.gradle.enterprise") version "3.11.1"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
 gradleEnterprise {
