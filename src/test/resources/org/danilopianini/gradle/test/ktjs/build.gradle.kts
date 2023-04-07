8deb8fe21c2f070dde021b27d7b1726488af1899
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-04-07 16:12:01 +0200

    chore(test): add kt-js project in tests

---
+++ src/test/resources/org/danilopianini/gradle/test/ktjs/build.gradle.kts
@@ -0,0 +1,32 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.danilopianini.gradle.mavencentral.DocStyle
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlin.js)
+    id("org.danilopianini.publish-on-central")
+}
+
+group = "org.danilopianini"
+version = "1.0.0"
+
+kotlin {
+    js {
+        browser()
+        nodejs()
+        binaries.library()
+    }
+    sourceSets {
+        val main by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+        val test by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
