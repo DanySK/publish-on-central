0f4321a1386d9a97be9a2469f1ba6c0002627980
Danilo Pianini <danilo.pianini@unibo.it>
2024-11-20 18:02:02 +0100

    ci(release): publish plugins before publishing on Central

--- release.config.mjs
+++ release.config.mjs
@@ -1,8 +1,8 @@
 const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
-./gradlew uploadKotlin uploadPluginMavenToMavenCentralNexus uploadPublishOnCentralPluginPluginMarkerMavenToMavenCentralNexus releaseStagingRepositoryOnMavenCentral --parallel || exit 1
-./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 2  
+./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
+./gradlew uploadKotlin uploadPluginMavenToMavenCentralNexus uploadPublishOnCentralPluginPluginMarkerMavenToMavenCentralNexus releaseStagingRepositoryOnMavenCentral --parallel || exit 2
 ./gradlew publishKotlinOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository || true
 `
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
