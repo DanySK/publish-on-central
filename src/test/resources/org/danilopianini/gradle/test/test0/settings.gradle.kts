dcac7abfdd41308febb2a168e5952184124c222e
Danilo Pianini <danilo.pianini@gmail.com>
2024-12-24 00:29:37 +0100

    test: publish the build scan in test0

--- src/test/resources/org/danilopianini/gradle/test/test0/settings.gradle.kts
+++ src/test/resources/org/danilopianini/gradle/test/test0/settings.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    id("com.gradle.develocity") version "3.19"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
-
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        val inCI = System.getenv("CI") == true.toString()
+        uploadInBackground = !inCI
+    }
+}
 rootProject.name = "test-publish-on-central"
