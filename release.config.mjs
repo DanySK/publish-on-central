51d86f2deabdc6e39b56042716ce67a026fe0d5e
Danilo Pianini <danilo.pianini@gmail.com>
2024-12-30 12:45:15 +0100

    chore(deps): upgrade publish-on-central to the latest beta version

--- release.config.mjs
+++ release.config.mjs
@@ -1,9 +1,9 @@
 const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
-./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
-./gradlew uploadKotlin uploadPluginMavenToMavenCentralNexus uploadPublishOnCentralPluginPluginMarkerMavenToMavenCentralNexus releaseStagingRepositoryOnMavenCentral --parallel || exit 2
-./gradlew publishKotlinOSSRHPublicationToGithubRepository publishPluginMavenPublicationToGithubRepository || true
+./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET  --parallel || exit 1
+./gradlew uploadAllToMavenCentralNexus releaseStagingRepositoryOnMavenCentral --parallel || exit 2
+./gradlew publishAllPublicationsToGithubRepository  --parallel || true
 `
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
