14e8dc0d1438a1924aebf851a02112cb4a23edee
Danilo Pianini <danilo.pianini@unibo.it>
2025-06-11 13:09:22 +0200

    ci: fix the release task name

--- release.config.mjs
+++ release.config.mjs
@@ -2,8 +2,8 @@ const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
 ./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
-./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseStagingRepositoryOnMavenCentral || exit 2
-./gradlew publishAllPublicationsToGithubRepository  --parallel || true
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseMavenCentralPortalPublication || exit 2
+./gradlew publishAllPublicationsToGithubRepository || true
 `
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
