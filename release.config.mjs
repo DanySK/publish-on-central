d0a0bed22e41e50d6aaec15691ca66467306718c
Danilo Pianini <danilo.pianini@unibo.it>
2025-06-11 12:07:59 +0200

    ci: try a beta release

--- release.config.mjs
+++ release.config.mjs
@@ -1,8 +1,8 @@
 const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
-./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET  --parallel || exit 1
-./gradlew uploadAllToMavenCentralNexus releaseStagingRepositoryOnMavenCentral --parallel || exit 2
+./gradlew publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET || exit 1
+./gradlew publishAllPublicationsToProjectLocalRepository zipMavenCentralPortalPublication releaseStagingRepositoryOnMavenCentral || exit 2
 ./gradlew publishAllPublicationsToGithubRepository  --parallel || true
 `
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
