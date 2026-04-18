71d0d03efbfa0a68667c7fcee7c2b6f13de39531
Danilo Pianini <danilo.pianini@unibo.it>
2026-04-19 01:52:09 +0200

    chore(deps): update plugin kotlin-qa to v1.4.0

--- src/main/kotlin/org/danilopianini/gradle/mavencentral/tasks/JarWithClassifier.kt
+++ src/main/kotlin/org/danilopianini/gradle/mavencentral/tasks/JarWithClassifier.kt
@@ -9,6 +9,8 @@ import org.gradle.work.DisableCachingByDefault
  * [org.gradle.api.file.DuplicatesStrategy.WARN].
  */
 @DisableCachingByDefault(because = "Caching behavior has not been reviewed for this custom Jar task yet")
+// Gradle task types intentionally remain abstract so Gradle can generate decorated task implementations.
+@Suppress("detekt.AbstractClassCanBeConcreteClass")
 abstract class JarWithClassifier(classifier: String) : Jar() {
     init {
         archiveClassifier.set(classifier)
