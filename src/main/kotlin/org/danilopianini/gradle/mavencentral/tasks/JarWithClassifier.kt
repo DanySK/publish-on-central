69537671b7fb004fc7b1ea993f58ae4e6af5d0a1
Danilo Pianini <danilo.pianini@unibo.it>
2026-04-19 02:14:28 +0200

    chore(core-deps): update gradle to v9.4.1 (#1669)

--- src/main/kotlin/org/danilopianini/gradle/mavencentral/tasks/JarWithClassifier.kt
+++ src/main/kotlin/org/danilopianini/gradle/mavencentral/tasks/JarWithClassifier.kt
@@ -2,11 +2,13 @@ package org.danilopianini.gradle.mavencentral.tasks
 
 import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.tasks.bundling.Jar
+import org.gradle.work.DisableCachingByDefault
 
 /**
  * A [Jar] task with the specified classifier, and adopting the duplicate strategy
  * [org.gradle.api.file.DuplicatesStrategy.WARN].
  */
+@DisableCachingByDefault(because = "Caching behavior has not been reviewed for this custom Jar task yet")
 abstract class JarWithClassifier(classifier: String) : Jar() {
     init {
         archiveClassifier.set(classifier)
